b8538a46f4ecb46621b9fd86151ef28f52455bea
tkwatanabe <tkwatanabe@shinwart.co.jp>
2017-06-20 07:23:00 +0900

    Initial Commit

---
+++ GourmetSearch2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GourmetSearch2
+//
+//  Created by tkwatanabe on 2017/06/20.
+//  Copyright © 2017年 tkwatanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
